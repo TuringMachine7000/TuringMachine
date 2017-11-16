4af6164ed1a94660757303cf3dd67bc8ef9342b0
TuringMachine7000 <turing.machine.7000@gmail.com>
2017-11-16 13:18:18 +0300

    fixed model + test

--- main.pas
+++ main.pas
@@ -5,10 +5,16 @@ unit main;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  TuringMod;
 
 type
+
+  { TForm1 }
+
   TForm1 = class(TForm)
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
   private
     { private declarations }
   public
@@ -21,6 +27,36 @@ var
 implementation
 
 {$R *.lfm}
+procedure test;
+    var theMachine : TuringMachine;
+    c1 : TuringCommand;
+    i : Integer;
+    begin
+    TheMachine := TuringMachine.Create;
+
+    c1 := TuringCommand.Create;
+    c1.currState:=1;
+    c1.currSymbol:='1';
+    c1.nextState:=1;
+    c1.nextSymbol:='0';
+    c1.orientation:=1;
+    Themachine.addCommand(c1);
+
+    TheMachine.start('111111');
+    for i := 1 to 10 do
+    begin
+         //ShowMessage('кря');
+         TheMachine.step;
+         ShowMessage(IntToStr(TheMachine.getState) + ' '+ TheMachine.getTape);
+    end;
+    end;
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  test;
+end;
 
 end.
 
